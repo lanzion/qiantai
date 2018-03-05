bd9fa82bf617cc559a28d3f978809002cc28f063
lzy <407223277@qq.com>
2018-03-05 19:57:51 +0800

    基本架构搭建完成

--- src/main.js
+++ src/main.js
@@ -6,6 +6,24 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import 'normalize.css';
+
+import axios from "axios";
+import api, { domain } from "./js/api.js";
+
+import "./less/index.less";
+
+// 配置默认域名, 这样请求的时候就不用在url里加域名了
+axios.defaults.baseURL = domain;
+// 因为我们调用的接口跨域了，默认情况下接口给我们设置的cookie无效，为了有效，我们要设置
+// 我们是跨域请求的接口, 默认不会携带cookie等信息, 后端需要这些信息来判断登陆状态, 所以要设为true
+axios.defaults.withCredentials = true;
+// 为了使用方便，把axios和pai添加到Vue原型，将来Vue组件可以直接使用
+
+Vue.prototype.$http = axios;
+Vue.prototype.$api = api;
+
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+// 导入账户管理相关页面
+import Login from "../components/account/login/Login.vue";
+
+// 导入商城相关页面
+import Shop from "../components/shop/Shop.vue";
+import Shopcart from "../components/shop/shopcart/Shopcart.vue";
+import GoodsList from "../components/shop/goods/List.vue";
+import GoodsDetail from "../components/shop/goods/Detail.vue";
+import OrderComlete from "../components/shop/order/Complete.vue";
+import OrderPay from "../components/shop/order/Pay.vue";
+import OrderSite from "../components/shop/order/Site.vue";
 
 Vue.use(Router)
 
+let goods = [
+  { name: 'GoodsList', path: 'goods/list', component: GoodsList },
+  { name: 'GoodsDetail', path: 'goods/detail/:id', component: GoodsDetail }
+]
+
+let order = [
+  { name: 'OrderComlete', path: 'order/complete', component: OrderComlete },
+  { name: 'OrderPay', path: 'order/pay/:id', component: OrderPay },
+  { name: 'OrderSite', path: 'order/site/:id', component: OrderSite }
+]
+
+let shopcart = [
+  { name: 'shopcart', path: 'shopcart', component: Shopcart },
+]
+
+
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    
+    //  账号管理路由配置
+    { name: 'login', path: '/login', component: Login },
+
+    // 商城路由配置
+    { name: 'shop', path: '/', component: Shop,children:[...goods,...order,...shopcart]}
   ]
 })
