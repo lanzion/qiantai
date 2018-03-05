bc668262d195b11276715304c058039fc384c6b3
lzy <407223277@qq.com>
2018-03-05 16:19:55 +0800

    初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Zion World'
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a{
+  text-decoration: none;
+  color:#fff;
+}
+</style>
